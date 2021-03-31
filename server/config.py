35a663f6dd2f9fbafda936a97d2b91271c303481
Andrei Rusau <andrei.rusau@yearend.com>
2021-03-31 15:38:07 -0400

    chore: removed .env with .env.sample; moved SECRET_KEY to .env

--- server/config.py
+++ server/config.py
@@ -9,7 +9,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'yo4p^5_yqv$gg7+o92+7ja154u7*)xj%jay&^!16su%(e#sua%'
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REMEMBER_COOKIE_HTTPONLY = True
