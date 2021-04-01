3dbf2c8ca7912c08c8e404ea10d04ca8e6e2b74a
Andrei Rusau <andrei.rusau@yearend.com>
2021-04-01 16:45:05 -0400

    fix: cors support added

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from models import db, login_manager
 from api.home_handler import home_handler
@@ -9,6 +10,10 @@ from api.auth_handler import auth_handler
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+cors = CORS(app, resources={r"/*": {
+    "origins": os.environ['FRONTEND_URL'],
+    "supports_credentials": True,
+}})
 
 login_manager.init_app(app)
 db.init_app(app)
