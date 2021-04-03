9ab330a4f956fc3fd73d95dfeb3c0cbd5ffd1447
Andrei Rusau <andrei.rusau@yearend.com>
2021-04-02 21:32:17 -0400

    feat: conversation endpoints added

---
+++ server/api/conversation_handler.py
@@ -0,0 +1,57 @@
+from flask import jsonify, request, Blueprint
+from flask_login import login_required, current_user
+
+from models import db, Conversation, Message, User
+
+conversation_handler = Blueprint('conversation_handler', __name__)
+
+
+@conversation_handler.route('/conversations', methods=['GET', 'POST'])
+@login_required
+def conversations():
+    if request.method == 'GET':
+        return jsonify(current_user.get_user_conversations())
+    else:
+        user_id = request.json.get('user_id', '')
+        id_error_message = 'Valid user ID must be provided'
+
+        if not user_id or int(user_id) == current_user.id:
+            return id_error_message, 400
+
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            return id_error_message, 400
+
+        conversation = Conversation.query.filter(Conversation.users.any(id=user.id),
+                                                 Conversation.users.any(id=current_user.id)).first()
+        if conversation:
+            return 'Conversation already exists', 409
+
+        conversation = Conversation()
+        conversation.users.extend([user, current_user])
+        db.session.add(conversation)
+        db.session.commit()
+        return '', 201
+
+
+@conversation_handler.route('/conversations/<int:conversation_id>/messages',
+                            methods=['GET', 'POST'])
+@login_required
+def messages(conversation_id):
+    conversation = Conversation.query.filter((Conversation.id == conversation_id),
+                                             Conversation.users.any(id=current_user.id)).first()
+
+    if not conversation:
+        return 'Conversation not found', 404
+
+    if request.method == 'GET':
+        return jsonify(conversation.get_conversation_messages())
+    else:
+        text = request.json.get('text', '')
+        if not text:
+            return 'Text is required', 400
+
+        message = Message(conversation_id=conversation.id, author_id=current_user.id, text=text)
+        db.session.add(message)
+        db.session.commit()
+        return '', 201
--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from models import db, login_manager
 from api.home_handler import home_handler
 from api.auth_handler import auth_handler
+from api.conversation_handler import conversation_handler
 
 
 app = Flask(__name__)
@@ -15,3 +16,4 @@ db.init_app(app)
 
 app.register_blueprint(home_handler)
 app.register_blueprint(auth_handler)
+app.register_blueprint(conversation_handler)
