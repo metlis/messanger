24a68317c75db39b036029e45b5007e74fec3ba5
Andrei Rusau <andrei.rusau@yearend.com>
2021-04-02 22:52:25 -0400

    feat: user search added

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ from models import db, login_manager
 from api.home_handler import home_handler
 from api.auth_handler import auth_handler
 from api.conversation_handler import conversation_handler
+from api.user_search_handler import user_search_handler
 
 
 app = Flask(__name__)
@@ -17,3 +18,4 @@ db.init_app(app)
 app.register_blueprint(home_handler)
 app.register_blueprint(auth_handler)
 app.register_blueprint(conversation_handler)
+app.register_blueprint(user_search_handler)
